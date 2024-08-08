e159773b843624e87f939cc8c01f9361ea9b6a74
zubeir <zubwd1@gmail.com>
2024-08-08 15:32:27 +0100

    First Commit

---
+++ src/components/EmployeeList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h2>Liste des employés</h2>
+    <ol>
+      <li v-for="employee in employees" :key="employee.id" @mouseover="hover = employee.id" @mouseleave="hover = null" :class="{ hover: hover === employee.id }">
+        {{ employee.name }}
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      hover: null,
+      employees: [
+        { id: 1, name: 'Aichetou' },
+        { id: 2, name: 'sidi' },
+        { id: 3, name: 'Ousmane' }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+ol {
+  padding: 0;
+  list-style: none;
+}
+
+li {
+  padding: 10px;
+  margin: 5px 0;
+  background-color: #f0f0f0;
+  cursor: pointer;
+}
+
+.hover {
+  background-color: rgb(31, 141, 31);
+}
+h2{
+  color : green;
+}
+</style>
